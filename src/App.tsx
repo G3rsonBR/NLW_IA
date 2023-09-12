2656d74ace20d7dbc80fa71c44398e77e4855e7a
G3rsonBR <gersons.br13@gmail.com>
2023-09-11 22:10:20 -0300

    Promp content & result layout - Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
-     <Button>Teste</Button>
+    <div className="min-h-screen flex flex-col">
+      <div  className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.AI</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido no NLW da Rockeatseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />  
+            GitHub
+          </Button>
+        </div>
+      </div>
+      
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed" 
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+            />
+          </div>
+          
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80"></aside>
+
+      </main>
+
+    </div>
   )
 }
